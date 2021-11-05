f6f01eb00cf0521397c26626d8e194dd3f0711f0
Jake Poshepny <jakeposhepny@jakes-mbp.home>
2021-11-05 00:43:13 -0500

    Update

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Tilty from 'react-tilty';
-import logo from '../../logo.png'
+import logo from '../../logo.png';
+import './Logo.css';
 
 // {<a target="_blank" href="https://icons8.com/icon/2070/brain">Brain</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a>}
 
@@ -9,7 +10,7 @@ const Logo = () => {
 		<div className='flex justify-center'>
 			<Tilty className='flex items-center justify-center mb3' options={{max: 25}} style={{ height: 150, width: 150, transformStyle: 'preserve-3d'}} glare={true} maxGlare={.5}>
 				<div className='absolute bg-white o-10' style={{ height: 150, width: 150}}></div>
-				<img src={logo} alt='logo' style={{ transform: 'translateZ(30px)' }}/>
+				<img className='logo' src={logo} alt='logo'/>
   				{/*<div className='b white f1' style={{ transform: 'translateZ(30px)' }}>FR</div>*/}
 			</Tilty>
 		</div>
--- src/node/script.js
+++ src/node/script.js
@@ -3,8 +3,6 @@ const script2 = require('./script2.js')
 const a = script2.largeNumber;
 const b = 5;
 
-// setTimeout(() => {
-// 	console.log(a + b)
-// }, 3000)
-
-console.log(a + b);
+setTimeout(() => {
+	console.log(a + b)
+}, 3000)
